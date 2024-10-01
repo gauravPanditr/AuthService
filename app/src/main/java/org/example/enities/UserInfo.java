72627d2d03e7405bd5c420ac39093e2402bfb35d
gauravPanditr <gauravpandit7856@gmail.com>
2024-10-01 17:05:12 +0530

    making enities

---
+++ app/src/main/java/org/example/enities/UserInfo.java
@@ -0,0 +1,31 @@
+package org.example.enities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+public class UserInfo {
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+
+    private Set<UserRole> roles = new HashSet<>();
+
+
+
+
+
+}
